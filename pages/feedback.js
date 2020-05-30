3b6f10e0dbaac2c24bc3ef292c9283930c27fde7
Sean Grove <sean@bushi.do>
2020-05-29 19:56:01 -0700

    More polish

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,53 +1,54 @@
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import { getAllIssues } from "../lib/api";
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllIssues } from '../lib/api'
 import {
   ONE_GRAPH_APP_ID,
   ONE_GRAPH_SERVER_SIDE_ACCESS_TOKEN,
-} from "../lib/constants";
-import Head from "next/head";
+} from '../lib/constants'
+import Head from 'next/head'
 import {
   auth,
   destroyAuth,
   saveAuth,
   useAuthGuardian,
   useFetchSupportedServices,
-} from "../lib/oneGraphNextClient";
-import { serverSideAuthTokenConfigurationPrompt } from "../lib/metaHelpers";
-import useSWR from "swr";
+} from '../lib/oneGraphNextClient'
+import { serverSideAuthTokenConfigurationPrompt } from '../lib/metaHelpers'
+import useSWR from 'swr'
 
 const submitFeedback = async ({ title, body, emotion }) => {
-  const issue = { title, body, emotion };
-  const result = await fetch("/api/feedback", {
-    method: "POST",
+  const issue = { title, body, emotion }
+  const result = await fetch('/api/feedback', {
+    method: 'POST',
     body: JSON.stringify(issue),
-  });
-  const json = await result.json();
-  return json;
-};
+  })
+  const json = await result.json()
+  return json
+}
 
 const submitStatusMessages = {
   idle: (
-    <>
-      What do <em>you</em> think of Next.js and AuthGuardian?
-    </>
+    <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+      What do <span className="font-bold">you</span> think of Next.js and
+      AuthGuardian?
+    </h1>
   ),
-  loading: "Submitting...",
-  success: "Submitted, thank you!",
+  loading: 'Submitting...',
+  success: 'Submitted, thank you!',
   error: serverSideAuthTokenConfigurationPrompt(ONE_GRAPH_APP_ID),
-};
+}
 
 export default function Index() {
-  const [submitStatus, setSubmitStatus] = React.useState("idle");
+  const [submitStatus, setSubmitStatus] = React.useState('idle')
 
   const [feedback, setFeedback] = React.useState({
-    title: "",
-    body: "",
-    emotion: "neutral",
-  });
+    title: '',
+    body: '',
+    emotion: 'neutral',
+  })
 
   return (
     <>
@@ -55,66 +56,75 @@ export default function Index() {
         <Head>
           <title>Next.js Auth Playground with OneGraph's AuthGuardian</title>
         </Head>
-        <Container>
+        <div className="flex flex-col mx-auto px-5">
+          {' '}
           {submitStatusMessages[submitStatus]}
           <br />
           <label>
-            Quick feedback?
+            <h2 className="text-1xl font-bold tracking-tighter leading-tight md:leading-none text-center md:text-left">
+              Quick feedback
+            </h2>
             <input
-              className="card"
+              className="card w-full"
               type="text"
               defaultValue={feedback.title}
               onChange={(event) => {
-                const title = event.target.value;
+                const title = event.target.value
                 setFeedback((oldFeedback) => {
-                  return { ...oldFeedback, title: title };
-                });
+                  return { ...oldFeedback, title: title }
+                })
               }}
             />
           </label>
           <br />
           <label>
-            Details (if you want!):
+            <h2 className="text-1xl font-bold tracking-tighter leading-tight md:leading-none text-center md:text-left">
+              Details (if you want!):
+            </h2>
             <textarea
               className="card"
               rows={4}
               defaultValue={feedback.body}
               onChange={(event) => {
-                const body = event.target.value;
+                const body = event.target.value
                 setFeedback((oldFeedback) => {
-                  return { ...oldFeedback, body: body };
-                });
+                  return { ...oldFeedback, body: body }
+                })
               }}
             ></textarea>
           </label>
           <label>
-            Your emotion:
+            <h2 className="text-1xl font-bold tracking-tighter leading-tight md:leading-none text-center md:text-left">
+              How're you feeling right now?
+            </h2>
             <select
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               value={feedback.emotion}
               onChange={(event) => {
-                const emotion = event.target.value;
+                const emotion = event.target.value
                 setFeedback((oldFeedback) => {
-                  return { ...oldFeedback, emotion: emotion };
-                });
+                  return { ...oldFeedback, emotion: emotion }
+                })
               }}
             >
-              <option value="meh">Meh</option>
-              <option value="neutral">Neutral</option>
-              <option value="amazed">Amazed</option>
-              <option value="blown-away">Completely blown away!</option>
+              <option value="meh">😑 Meh</option>
+              <option value="neutral">😐 Neutral</option>
+              <option value="amazed">😲 Amazed</option>
+              <option value="blown-away">🤯 Completely blown away!</option>
             </select>
           </label>
           <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
             onClick={async () => {
-              setSubmitStatus("loading");
-              const result = await submitFeedback(feedback);
-              const newStatus = result.success ? "success" : "error";
-              setSubmitStatus(newStatus);
+              setSubmitStatus('loading')
+              const result = await submitFeedback(feedback)
+              const newStatus = result.success ? 'success' : 'error'
+              setSubmitStatus(newStatus)
             }}
           >
-            Submit{" "}
+            Submit{' '}
           </button>
-        </Container>
+        </div>
       </Layout>
 
       <style jsx>{`
@@ -271,11 +281,11 @@ export default function Index() {
         }
       `}</style>
     </>
-  );
+  )
 }
 
 export async function getStaticProps() {
   return {
     props: {},
-  };
+  }
 }
